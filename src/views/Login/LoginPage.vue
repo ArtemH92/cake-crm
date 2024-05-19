f89e1c3078dd858049f3994ef76d53adcc3948d2
Ärtem Khominski <a.hominski@yandex.ru>
2024-05-19 14:04:26 +0300

    add login add register pages

--- src/views/Login/LoginPage.vue
+++ src/views/Login/LoginPage.vue
@@ -1,7 +1,38 @@
+<script setup>
+import { reactive } from 'vue'
+import { message, Card, Form, FormItem, Input, InputPassword, Button, Typography } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+
+const user = reactive({
+  username: '',
+  password: ''
+})
+
+const router = useRouter()
+const loginHandler = (user) => {
+  if(user.username === 'artem' && user.password === '123') {
+    message.success('Вы успешно авторизовались')
+    router.push('/')
+  } else {
+    message.error('Данные введены не верно')
+  }
+}
+
+</script>
+
 <template>
-  <div>Login</div>
+  <Card title="Авторизация" class="min-w-[600px]">
+    <Form :model="user" autocomplete="off" label-align="left" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }">
+      <FormItem label="Введите имя пользователя" name="username">
+        <Input v-model:value="user.username" />
+      </FormItem>
+      <FormItem label="Введите пароль" name="password">
+        <InputPassword v-model:value="user.password" />
+      </FormItem>
+      <FormItem class="flex justify-center">
+        <Button type="primary" @click="loginHandler(user)">Войти</Button>
+      </FormItem>
+    </Form>
+    <Typography class="text-center">У Вас нет аккаунта? <router-link to="/register">Зарегистрироваться</router-link></Typography>
+  </Card>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
--- src/views/Register/RegisterPage.vue
+++ src/views/Register/RegisterPage.vue
@@ -1,5 +1,42 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import { message, Card, Form, FormItem, Input, InputPassword, Button, Typography } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+
+const user = reactive({
+  username: '',
+  password: '',
+  comfirmPassword: ''
+})
+
+const router = useRouter()
+const loginHandler = (user) => {
+  if(user.username === 'artem' && user.password === '123') {
+    message.success('Вы успешно авторизовались')
+    router.push('/')
+  } else {
+    message.error('Данные введены не верно')
+  }
+}
+
+</script>
 
 <template>
-  <div>Register</div>
+  <Card title="Регистрация" class="min-w-[600px]">
+    <Form :model="user" autocomplete="off" label-align="left" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }">
+      <FormItem label="Введите имя пользователя" name="username">
+        <Input v-model:value="user.username" />
+      </FormItem>
+      <FormItem label="Введите пароль" name="password">
+        <InputPassword v-model:value="user.password" />
+      </FormItem>
+      <FormItem label="Подтвердите пароль" name="password">
+        <InputPassword v-model:value="user.comfirmPassword" />
+      </FormItem>
+      <FormItem class="flex justify-center">
+        <Button type="primary" @click="loginHandler(user)">Зарегистрироваться</Button>
+      </FormItem>
+    </Form>
+    <Typography class="text-center">Вы зарегистрированы? <router-link to="/login">Войти</router-link></Typography>
+  </Card>
 </template>
